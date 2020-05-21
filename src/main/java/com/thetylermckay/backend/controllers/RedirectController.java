ba74a26e2cb878e0ba979a9cadbf76e410048ca3
Tyler McKay <tmckay1.jackson@gmail.com>
2020-05-20 21:42:01 -0400

    fix redirect

--- src/main/java/com/thetylermckay/backend/controllers/RedirectController.java
+++ src/main/java/com/thetylermckay/backend/controllers/RedirectController.java
@@ -2,11 +2,15 @@ package com.thetylermckay.backend.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RedirectController {
 
-  @RequestMapping(value = "/**/{[path:[^\\.]*}")
+  @RequestMapping(
+      value = {"/{path:[^\\.]*}", "/**/{path:^(?!oauth).*}/{path:[^\\.]*}"},
+      method = RequestMethod.GET
+  )
   public String redirect() {
     return "forward:/index.html";
   }
